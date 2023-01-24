ce004c9ba253bc86ab18b3a35082d77f958f8be7
Danyos <daniel.hambardzumyan@mail.ru>
2023-01-24 21:21:59 +0400

    array obj

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
 function App() {
-  return (
-      <div>
-        <h1>Barev bolorit</h1>
-      </div>
-  )
-}
+    const x = "muxi"
+    let info = [
+        {
+            id:1,
+            name:"valod",
+            surname:"karapetyan"
+        },
+        {
+            id:2,
+            name:"Gaspar",
+            surname:"karapetyan"
+        },
+        {
+            id:3,
+            name:"Muxi",
+            surname:"karapetyan"
+        },
 
+    ]
+
+
+    return (
+        <>
+            <h1 className="titles">Barev {x}</h1>
+            <div>
+                {info.map((res)=>(
+                     <div key={res.id}>
+                         <h6>{res.name}</h6>
+                         <h6>{res.surname}</h6>
+                     </div>
+                ))
+                }
+            </div>
+
+        </>
+    )
+}
 export default App;
+
+
